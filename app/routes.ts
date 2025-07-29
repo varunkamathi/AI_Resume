174bab4ebeb750632313a99696c75d207676f232
varunkamathi <varunkamathi522004@gmail.com>
2025-07-29 11:55:13 +0530

    create upload pdf logic

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index , route} from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route('/auth', "routes/Auth.tsx"),
-    //route('/upload', "app/routes/upload.tsx"),
+    route('/upload', "routes/Upload.tsx"),
     //route('/resume/:id', "app/routes/resume.tsx"),
     //route('/wipe', "app/routes/wipe.tsx"),
 ] satisfies RouteConfig;

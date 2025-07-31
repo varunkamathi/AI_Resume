9acc2b1984da6df8b72e865002e256061fe8f23b
varunkamathi <varunkamathi522004@gmail.com>
2025-07-31 19:39:40 +0530

    completed Ui resume routes

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,9 +1,9 @@
 import {Link, useNavigate, useParams} from "react-router";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "../lib/Puter";
-//import Summary from "~/components/Summary";
-//import ATS from "~/components/ATS";
-//import Details from "~/components/Details";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 export const meta = () => ([
     { title: 'Resumind | Review ' },
@@ -75,10 +75,10 @@ const resume = () => {
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                           {/*  <Summary feedback={feedback} />
+                             <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
-                            */}
+                            *
                         </div>
                     ) : (
                         <img src="/images/resume-scan-2.gif" className="w-full" />
---
+++ app/routes/wipe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const wipe = () => {
+  return (
+    <div>wipe</div>
+  )
+}
+
+export default wipe
